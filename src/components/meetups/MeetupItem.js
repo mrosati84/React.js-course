959c26ea151aecc683c747efd1c40dcc6f2991f9
mrosati84 <rosati.matteo@gmail.com>
2023-08-25 13:06:45 +0200

    add Card component

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,13 +1,17 @@
+import Card from '../ui/Card.js';
+
 function MeetupItem(props) {
   return (
     <li key={props.meetup.id}>
-      <h3>{props.meetup.title}</h3>
-      <div>
-        <img height="150" src={props.meetup.image} alt="" />
-      </div>
-      <div>
-        <button>Add to favorites</button>
-      </div>
+      <Card>
+        <h3>{props.meetup.title}</h3>
+        <div>
+          <img height="150" src={props.meetup.image} alt="" />
+        </div>
+        <div>
+          <button>Add to favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
