d103b2461de3f92851dfe1f54660c6b5814ce53a
mrosati84 <rosati.matteo@gmail.com>
2023-08-25 12:41:59 +0200

    add a max height to images

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -3,7 +3,7 @@ function MeetupItem(props) {
     <li key={props.meetup.id}>
       <h3>{props.meetup.title}</h3>
       <div>
-        <img src={props.meetup.image} alt="" />
+        <img height="150" src={props.meetup.image} alt="" />
       </div>
       <div>
         <button>Add to favorites</button>
